b847dfca87d3f51ea3efe60bddd4c52d91b58b51
Brian Simmons <bsimmonsn@gmail.com>
2016-08-18 12:48:24 -0500

    fixing heroku javascript issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,36 +2,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/js/bootstrap
-//= require bootstrap/js/bootstrap.min
-//= require bootstrap/js/npm
 
-//= require contact_form/contact
-//= require js/wow.min
+
+
+
 
 //= require js/jquery.mb.YTPlayer
 //= require js/jquery.min
-
-//= require js/masonry.pkgd.min
-
-//= require js/revolution-custom
-
-//= require lightbox2/Gruntfile
+//= require bootstrap/js/bootstrap
+//= require bootstrap/js/bootstrap.min
+//= require bootstrap/js/npm
+//= require js/wow.min
+//= require js/jquery.stellar.min
+//= require flexslider/jquery.flexslider
+//= require flexslider/jquery.flexslider-min
+//= require contact_form/contact
 //= require lightbox2/dist/js/lightbox-plus-jquery
 //= require lightbox2/dist/js/lightbox-plus-jquery.min
 //= require lightbox2/dist/js/lightbox
 //= require lightbox2/dist/js/lightbox.min
 //= require lightbox2/src/js/lightbox
-//= require js/jquery.stellar.min
+//= require lightbox2/Gruntfile
+//= require js/masonry.pkgd.min
 
-
+//= require js/revolution-custom
 
 //= require js/custom
-
-
-
-//= require flexslider/jquery.flexslider
-//= require flexslider/jquery.flexslider-min
 
 
 
