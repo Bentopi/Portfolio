64c816f8e3a996a238bde00710218b7c69f3a178
Brian Simmons <bsimmonsn@gmail.com>
2016-08-18 12:24:33 -0500

    Portfolio first commit"

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,42 @@
+
+//= require jquery
+
+//= require jquery_ujs
+
+
+//= require bootstrap-sprockets
+
+//= require turbolinks
+//= require bootstrap/js/bootstrap
+//= require bootstrap/js/bootstrap.min
+
+//= require contact_form/contact
+//= require js/wow.min
+
+//= require js/jquery.mb.YTPlayer
+//= require js/jquery.min
+
+//= require js/masonry.pkgd.min
+
+//= require js/revolution-custom
+
+//= require lightbox2/Gruntfile
+//= require lightbox2/dist/js/lightbox-plus-jquery
+//= require lightbox2/dist/js/lightbox-plus-jquery.min
+//= require lightbox2/dist/js/lightbox
+//= require lightbox2/dist/js/lightbox.min
+//= require lightbox2/src/js/lightbox
+//= require js/jquery.stellar.min
+
+
+
+//= require js/custom
+//= require bootstrap/js/npm
+
+
+//= require flexslider/jquery.flexslider
+//= require flexslider/jquery.flexslider-min
+
+
+
+//= require_tree .
