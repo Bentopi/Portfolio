911e1f75cec7a1f70ddf0a6a83ad4431e8652b58
Brian Simmons <bsimmonsn@gmail.com>
2016-08-18 12:58:30 -0500

    fix js issues heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,25 +7,14 @@
 
 
 
-//= require js/jquery.mb.YTPlayer
+
 //= require js/jquery.min
-//= require bootstrap/js/bootstrap
 //= require bootstrap/js/bootstrap.min
-//= require bootstrap/js/npm
 //= require js/wow.min
 //= require js/jquery.stellar.min
-//= require flexslider/jquery.flexslider
 //= require flexslider/jquery.flexslider-min
 //= require contact_form/contact
-//= require lightbox2/dist/js/lightbox-plus-jquery
-//= require lightbox2/dist/js/lightbox-plus-jquery.min
-//= require lightbox2/dist/js/lightbox
 //= require lightbox2/dist/js/lightbox.min
-//= require lightbox2/src/js/lightbox
-//= require lightbox2/Gruntfile
-//= require js/masonry.pkgd.min
-
-//= require js/revolution-custom
 
 //= require js/custom
 
